12e665f78e114d591bfdf8990db3d655edb96532
somnath796 <51814686+somnath796@users.noreply.github.com>
2021-07-04 13:00:56 +0530

    Transfer Learning and Stacking Ensemble codes

---
+++ Stacking_ensemble.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+import os
+from tensorflow import keras
+from tqdm import tqdm
+import tensorflow
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.applications.densenet import DenseNet121
+from tensorflow.keras.callbacks import *
+from tensorflow.keras import backend as K
+K.clear_session()
+import itertools
+import matplotlib.pyplot as plt
+import cv2
+import matplotlib.cm as cm
+
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer,LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+import tensorflow as tf
+from tensorflow.keras.applications import *
+import tensorflow as tf
+from sklearn.preprocessing import OneHotEncoder
+
+
+#Data to be loaded as X_train,Y_train and X_test,Y_test with onehot encoded labels
+
+image_width=224
+image_height=224
+no_of_channels=3
+input_shape=(image_width,image_height,no_of_channels)
+
+
+"""# **Evaluation**"""
+
+our_vgg = tf.keras.models.load_model('./vgg_best.h5')
+our_xcep = tf.keras.models.load_model('./xception_best.h5')
+our_res = tf.keras.models.load_model('./resnetbest.h5')
+
+print("-"*30)
+print("VGG16")
+print(our_vgg.evaluate(X_test,Y_test))
+print("-"*30)
+print("Xception")
+print(our_xcep.evaluate(X_test,Y_test))
+print("-"*30)
+print("ResNet50")
+print(our_res.evaluate(X_test,Y_test))
+print("-"*30)
+
+our_vgg.trainable = False
+our_xcep.trainable = False
+our_res.trainable = False
+
+#Delete unused variables and clear garbage values
+#del history1, model1, hist_df
+import gc
+
+gc.collect()
+
+"""# **Ensemble**"""
+
+def stacking_ensemble(members,input_shape,n_classes):
+    commonInput = Input(shape=input_shape)
+    out=[]
+
+    for model in members:    
+        model._name= model.get_layer(index = 0)._name +"-test"+ str(members.index(model)+1)
+        out.append(model(commonInput))
+
+    modeltmp = concatenate(out,axis=-1)
+    modeltmp = Dense(256, activation='relu')(modeltmp)
+    modeltmp = Dense(128, activation='relu')(modeltmp)
+    modeltmp = Dropout(0.1)(modeltmp)
+    modeltmp = Dense(n_classes, activation='softmax')(modeltmp)
+    stacked_model = Model(commonInput,modeltmp)
+    stacked_model.compile( loss='categorical_crossentropy',optimizer= optimizer, metrics=['accuracy'])
+
+    return stacked_model
+
+members =[our_vgg, our_xcep, our_res]
+
+n_classes=3
+batch=16
+optimizer= Adam(lr=5e-5, beta_1=0.9, beta_2=0.999)
+
+stacked = stacking_ensemble(members,(image_height,image_width,3),n_classes)
+print(stacked.summary())
+
+stacked_checkpoint = ModelCheckpoint("stacked_best.h5", monitor='val_accuracy', verbose=1,
+    save_best_only=True, mode='auto', period=1)
+
+stacked_hist = stacked.fit(X_train,Y_train,
+                            epochs=EPOCHS, #epochs,
+                            verbose=1,
+                            batch_size = 16,
+                            validation_data= (X_test,Y_test),
+                            class_weight = class_weight,
+                            callbacks=[reduce_learning_rate, stacked_checkpoint]) 
+
+
+
+stacked.evaluate(X_test, Y_test)
+
+del stacked_hist, stacked
+gc.collect()
+
+stacked = tf.keras.models.load_model("/content/stacked_best.h5")
+print("-.-"*30)
+print('Model Name: Stacked')
+print(stacked.evaluate(X_test, Y_test))
+print("-.-"*30)
+
+"""# **Finding F1 score etc.**"""
+
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.metrics import classification_report, confusion_matrix
+
+# True values
+y_true = Y_test.argmax(axis=-1)
+
+def confusion(model):
+  #Predicted values
+  prob = model.predict(X_test)
+  y_pred = prob.argmax(axis= -1)
+  # Print the confusion matrix
+  print("--"*30)
+  print(confusion_matrix(y_true, y_pred))
+  print("--"*30)
+  # Print the precision and recall, among other metrics
+  print(classification_report(y_true, y_pred, digits=6))
+  print("--"*30)
+
+our_models =  {"VGG16": our_vgg, "Xception": our_xcep, "ResNet50": our_res, "Stacked": stacked}
+for m in our_models:
+  print("--"*30)
+  print(m)
+  print("--"*30)
+  confusion(our_models[m])
---
+++ base_models.py
@@ -0,0 +1,165 @@
+
+
+# Commented out IPython magic to ensure Python compatibility.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+import os
+from tensorflow import keras
+from tqdm import tqdm
+import tensorflow
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.applications.densenet import DenseNet121
+from tensorflow.keras.callbacks import *
+from tensorflow.keras import backend as K
+K.clear_session()
+import itertools
+import matplotlib.pyplot as plt
+import cv2
+import matplotlib.cm as cm
+
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer,LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+import tensorflow as tf
+from tensorflow.keras.applications import *
+import tensorflow as tf
+from sklearn.preprocessing import OneHotEncoder
+
+
+#Data to be loaded as X_train,Y_train and X_test,Y_test with onehot encoded labels
+
+image_width=224
+image_height=224
+no_of_channels=3
+input_shape=(image_width,image_height,no_of_channels)
+
+
+
+reduce_learning_rate = ReduceLROnPlateau(monitor='val_loss',
+                                         factor=0.1,
+                                         patience=5,
+                                         cooldown=2,
+                                         min_lr=1e-8,
+                                         verbose=1)
+
+early_stop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto',
+    baseline=None, restore_best_weights=True
+)
+
+EPOCHS = 100
+
+"""# **Xception**"""
+
+
+tmodel_base = Xception(input_shape = input_shape, 
+                                include_top = False, 
+                                weights = 'imagenet')
+for layer in tmodel_base.layers:
+    layer.trainable = False
+
+#Getting desired layer output
+# Modification of pretrained model
+last_layer = tmodel_base.get_layer('block14_sepconv2_act')
+last_output = last_layer.output
+
+x = MaxPooling2D(strides=(2,2))(last_output)
+x = Flatten()(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.15)(x)
+x = Dense(64, activation='relu')(x)
+x = Dropout(0.25)(x)
+x = Dense(3, activation='softmax')(x)
+#Compiling model
+model2 = Model(inputs = tmodel_base.input, outputs = x, name = 'Our_Xception')
+model2.summary()
+
+opt1 = Adam(lr=1e-5, beta_1=0.9, beta_2=0.999)
+xception_checkpoint = ModelCheckpoint("xception_best.h5", monitor='val_accuracy', verbose=1,
+    save_best_only=True, mode='auto', period=1)
+
+model2.compile(optimizer = opt1 , loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+history2 = model2.fit(X_train, Y_train, epochs=EPOCHS, validation_data = (X_test,Y_test)
+                     ,class_weight=class_weight ,callbacks=[ xception_checkpoint])
+
+
+"""# **ResNet**"""
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+num_classes= 3
+tmodel_base = ResNet50(input_shape = input_shape, 
+                                include_top = False, 
+                                weights = 'imagenet')
+for layer in tmodel_base.layers:
+    layer.trainable = False
+
+last = tmodel_base.output
+
+
+x = Conv2D(1024,(2,2),strides=(1,1))(last)
+x = Flatten()(x)
+x = Dense(512, activation = 'relu')(x)
+x = Dropout(rate = 0.15)(x)
+x = Dense(256, activation = 'relu')(x)
+x = Dropout(rate = 0.25)(x)
+x = Dense(3, activation = 'softmax')(x)
+
+#Compiling model
+model3 = Model(inputs = tmodel_base.input, outputs = x)
+model3.summary()
+
+opt1 = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999)
+resnet_checkpoint = ModelCheckpoint("resnetbest.h5", monitor='val_accuracy', verbose=1,
+    save_best_only=True, mode='auto', period=1)
+
+model3.compile(optimizer = opt1 , loss = 'categorical_crossentropy', metrics = ['accuracy'])
+history3 = model3.fit(X_train, Y_train,epochs=EPOCHS, validation_data = (X_test, Y_test)
+                      ,class_weight=class_weight ,callbacks=[resnet_checkpoint])
+
+
+"""# **VGG16**"""
+
+from tensorflow.keras.applications import VGG16
+tmodel_base = VGG16(input_shape = input_shape, 
+                                include_top = False, 
+                                weights = 'imagenet')
+for layer in tmodel_base.layers:
+    layer.trainable = False
+
+#Getting desired layer output
+last_layer = tmodel_base.get_layer('block5_pool')
+last = last_layer.output
+
+x = Flatten()(last)
+x = Dense(512, activation = 'relu')(x)
+x = Dropout(rate = 0.25)(x)
+x = Dense(3, activation = 'softmax')(x)
+#Compiling model
+model1 = Model(inputs = tmodel_base.input, outputs = x, name = 'VGG16')
+opt1 = Adam(lr=1e-5, beta_1=0.9, beta_2=0.999)
+
+
+model1.compile(optimizer = opt1 , loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model1.summary()
+
+vgg_checkpoint = ModelCheckpoint("vgg_best.h5", monitor='val_loss', verbose=1,
+    save_best_only=True, mode='auto', period=1)
+
+history1 = model1.fit(X_train, Y_train,  epochs=EPOCHS, validation_data = (X_test, Y_test)
+                       ,class_weight=class_weight ,callbacks=[vgg_checkpoint])
+
+
+
